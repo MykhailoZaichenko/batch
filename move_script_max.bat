b108ab42123c9fb6c8382ef2372331c8b0cde897
MichailoZaichenko <michailo.zaichenko@gmail.com>
2025-04-07 15:29:22 +0300

    work with hide files

--- move_script_max.bat
+++ move_script_max.bat
@@ -13,11 +13,10 @@ if "%~1"=="-h" (
     echo Moves all files from the source folder to the destination, handling file attributes.
     echo Termination codes:
     echo "0 -> Successful move"
-    echo "1 -> Help displayed (-h ��� --help)"
+    echo "1 -> Help displayed (-h or --help)"
     echo "2 -> The output folder does not exist"
     echo "3 -> No destination folder specified"
     exit /b 1
-    echo Exit code %ERRORLEVEL%
 )
 
 if "%~1"=="--help" (
@@ -25,11 +24,10 @@ if "%~1"=="--help" (
     echo Moves all files from the source folder to the destination, handling file attributes.
     echo Termination codes:
     echo "0 -> Successful move"
-    echo "1 -> Help displayed (-h ��� --help)"
+    echo "1 -> Help displayed (-h or --help)"
     echo "2 -> The output folder does not exist"
     echo "3 -> No destination folder specified"
     exit /b 1
-    echo Exit code %ERRORLEVEL%
 )
 
 rem Get parameters
@@ -39,19 +37,16 @@ rem Shifting for recursive reading fileNames
 shift
 shift
 
-
 rem Check if source directory exists
 if not exist "%SourceDir%" (
     echo Error: Source folder does not exist.
     exit /b 2
-    echo Exit code %ERRORLEVEL%
 )
 
 rem Validate input parameters
 if "%TargetDir%"=="" (
     echo Error: Destination folder is not specified.
     exit /b 3
-    echo Exit code %ERRORLEVEL%
 )
 
 rem Create destination folder if it does not exist
@@ -72,21 +67,22 @@ set "SpecificFiles=1"
 
 if exist "%SourceDir%\%FileToMove%" (
     rem Retrieve file attributes
-    attrib "%SourceDir%\%FileToMove%" > nul
-    set "Hidden="
-    set "ReadOnly="
-    set "Archive="
-
-    for /f "tokens=2 delims= " %%A in ('attrib "%SourceDir%\%FileToMove%"') do (
-        if "%%A"=="H" set "Hidden=Hidden"
-        if "%%A"=="R" set "ReadOnly=ReadOnly"
-        if "%%A"=="A" set "Archive=Archive"
-    )
+    set "Attribs="
+    for /f "tokens=2 delims= " %%A in ('attrib "%SourceDir%\%FileToMove%"') do set "Attribs=!Attribs!%%A"
 
     rem Display file information before moving
-    echo Moving file: %FileToMove% [!Hidden! !ReadOnly! !Archive!]
+    echo Moving file: %FileToMove% [!Attribs!]
+
+    rem Temporarily remove hidden attribute
+    attrib -h "%SourceDir%\%FileToMove%"
 
     move "%SourceDir%\%FileToMove%" "%TargetDir%\" > nul
+
+    rem Restore hidden attribute if it was set
+    if not "!Attribs!"=="" (
+        attrib +h "%TargetDir%\%FileToMove%"
+    )
+    
     echo Moved: %FileToMove%
 ) else (
     echo File not found: %FileToMove%
@@ -97,26 +93,26 @@ goto move_file
 
 :check_all_files
 rem If no specific files were provided, move all files
-    if "%SpecificFiles%"=="0" (
-        echo Moving all files from "%SourceDir%" to "%TargetDir%" with attributes...
-        for %%F in ("%SourceDir%\*") do (
-            attrib "%%F" > nul
-            set "Hidden="
-            set "ReadOnly="
-            set "Archive="
+if "%SpecificFiles%"=="0" (
+    echo Moving all files from "%SourceDir%" to "%TargetDir%" with attributes...
+    for %%F in ("%SourceDir%\*") do (
+        set "Attribs="
+        for /f "tokens=2 delims= " %%A in ('attrib "%%F"') do set "Attribs=!Attribs!%%A"
 
-            for /f "tokens=2 delims= " %%A in ('attrib "%%F"') do (
-                if "%%A"=="H" set "Hidden=Hidden"
-                if "%%A"=="R" set "ReadOnly=ReadOnly"
-                if "%%A"=="A" set "Archive=Archive"
-            )
+        echo Moving file: %%~nxF [!Attribs!]
 
-            echo Moving file: %%~nxF [!Hidden! !ReadOnly! !Archive!]
-            move "%%F" "%TargetDir%\" > nul
+        rem Temporarily remove hidden attribute
+        attrib -h "%%F"
+
+        move "%%F" "%TargetDir%\" > nul
+
+        rem Restore hidden attribute if it was set
+        if not "!Attribs!"=="" (
+            attrib +h "%TargetDir%\%%~nxF"
         )
     )
+)
 
 rem Completion message
 echo Files have been moved to "%TargetDir%".
 exit /b 0
-echo Exit code %ERRORLEVEL%
